5e6dd4c9d02365f934aacbbd73ae18fbb0718a15
Ziath <ziathadam@gmail.com>
2025-06-01 16:07:27 +0700

    change axios utils into class

--- utils/axios.js
+++ utils/axios.js
@@ -1,37 +1,36 @@
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: process.env.OLLAMA_BASE_URL || "", // optional: can override per request
-  timeout: 10000, // 10 seconds
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+class HttpClient {
+  constructor(baseURL = "") {
+    this.instance = axios.create({
+      baseURL: baseURL, // Set from constructor parameter
+      timeout: 10000, // 10 seconds
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-// Optional: Add interceptors
-instance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    console.error("Axios error:", error?.response?.data || error.message);
-    return Promise.reject(error);
+    // Add interceptors
+    this.instance.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        console.error("Axios error:", error?.response?.data || error.message);
+        return Promise.reject(error);
+      }
+    );
   }
-);
 
-// Export a generic request wrapper
-export async function httpRequest({
-  method,
-  url,
-  data = null,
-  params = null,
-  headers = {},
-}) {
-  const response = await instance.request({
-    method,
-    url,
-    data,
-    params,
-    headers,
-  });
+  async request({ method, url, data = null, params = null, headers = {} }) {
+    const response = await this.instance.request({
+      method,
+      url,
+      data,
+      params,
+      headers,
+    });
 
-  return response.data; // Axios auto-parses JSON
+    return response.data;
+  }
 }
+
+export default HttpClient;
