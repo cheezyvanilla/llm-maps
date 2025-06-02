a08ed83a2e4b468dc86f9bf1943e14ebd6fbd65c
Ziath <ziathadam@gmail.com>
2025-06-02 07:53:15 +0700

    formatting

--- server/websocket/index.js
+++ server/websocket/index.js
@@ -1,18 +1,18 @@
-import { WebSocketServer } from 'ws'
-import chatHandler from '../api/chat/handler/chatHandler.js';
+import { WebSocketServer } from "ws";
+import ChatService from "../api/chat/handler/chatHandler.js";
 
 export default function createWebSocketServer(server) {
-    const wss = new WebSocketServer({ server })
+  const wss = new WebSocketServer({ server });
 
-    wss.on('connection', (ws) => {
-        console.log('client connected')
+  wss.on("connection", (ws) => {
+    console.log("client connected");
 
-        ws.on('message', async (message) => {
-            await chatHandler(ws, message)
-        });
+    ws.on("message", async (message) => {
+      await new ChatService().chatHandler(ws, message);
+    });
 
-        ws.on('close', () => {
-                console.log('Client disconnected');
-        })
-    })
+    ws.on("close", () => {
+      console.log("Client disconnected");
+    });
+  });
 }
