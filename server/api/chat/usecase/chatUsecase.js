1c40de9f9d237c8b3a81c3a77161ab3333e6b420
Ziath <ziathadam@gmail.com>
2025-06-02 09:24:31 +0700

    complete usecase layer

--- server/api/chat/usecase/chatUsecase.js
+++ server/api/chat/usecase/chatUsecase.js
@@ -1,4 +1,53 @@
-export default async function processMessage(message) {
-    // ask LLM to return a json with location and intent
+import ChatRepository from "../repository/chatRepository.js";
+export default class ChatUsecase {
+  constructor() {
+    this.repo = new ChatRepository();
+  }
 
+  async processMessage(message) {
+    try {
+      // send msg to ollama
+      const llmRes = await this.repo.askLLama(message);
+      const location = await this.repo.geocodeLocation(llmRes.location);
+      let query = {
+        lat: location.lat,
+        lng: location.lng,
+        type: llmRes.type,
+      };
+      const places = await this.repo.searchNearbyPlaces(query);
+
+      // format places response
+      const placesResponse = this.#formatPlacesResponse(places);
+      return placesResponse;
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  }
+
+  #formatPlacesResponse(places) {
+    if (!places.length) return "No places found nearby.";
+
+    // limit only to 10 places
+    if (places.length > 10) places = places.slice(0, 10);
+    const lines = ["Here are some places you might like:<br>"];
+
+    places.forEach((place) => {
+      const name = place.name;
+      const rating = place.rating || "N/A";
+      const total = place.user_ratings_total || 0;
+      const address = place.vicinity || "";
+      const closed = place.permanently_closed || false;
+      const emoji = place.types?.includes("restaurant") ? "🍽️" : "🏨";
+
+      let line = `${emoji} <b>${name}</b><br>⭐ ${rating} · ${total.toLocaleString()} ratings<br>📍 ${address}`;
+      if (closed) {
+        line = `⚠️ <b>${name}</b> (Temporarily Closed)<br>⭐ ${rating} · ${total.toLocaleString()} ratings<br>📍 ${address}`;
+      }
+
+      lines.push(line + "<br>");
+    });
+
+    return lines.join("<br>");
+  }
 }
