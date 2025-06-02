46854fdd6cf92467a5f6764a254aba033edf4f64
Ziath <ziathadam@gmail.com>
2025-06-02 07:52:10 +0700

    move websocket init to server

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-import http from 'http'
-import app from './server/index.js'
-import dotenv from 'dotenv'
-import createWebSocketServer from './server/websocket/index.js'
+import http from "http";
+import app from "./server/index.js";
+import dotenv from "dotenv";
+import createWebSocketServer from "./server/websocket/index.js";
 
-dotenv.config()
+dotenv.config();
 
-const port = process.env.PORT
-const server = http.createServer(app)
+const port = process.env.PORT;
+const server = http.createServer(app);
 
-createWebSocketServer(server)
 server.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
--- server/index.js
+++ server/index.js
@@ -1,16 +1,19 @@
-import express from 'express'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import createWebSocketServer from "./websocket";
 
-const app = express()
+const app = express();
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, '../client')))
+app.use(express.static(path.join(__dirname, "../client")));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/index.html'))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/index.html"));
+});
 
-export default app
+createWebSocketServer(app);
+
+export default app;
